d871f0e8fe1eea637339418611a33bc18f8643d4
Ron Tsabari <rontsabari@Rons-MacBook-Air.local>
2025-10-27 22:00:30 +0200

    fixing build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '127.0.0.1',
     port: parseInt(process.env.FRONTEND_PORT || '3000', 10),
     proxy: {
       '/hello': {
